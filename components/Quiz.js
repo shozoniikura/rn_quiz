ea6ac82993244bb965ed5cdfa32726bab8315529
NIIKURA Shozo <niikura@nyam.co.jp>
2020-10-24 17:24:35 +0900

    ナビゲーションのネストを行いました。

---
+++ components/Quiz.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import commonStyles from '../components/common';
+import {Card, Input} from 'react-native-elements';
+
+export default function Quiz() {
+  return (
+    <View>
+      <Card>
+        <Text>にゅうりょくしてください</Text>
+        <Input>aaa</Input>
+      </Card>
+    </View>
+  );
+}
--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -2,15 +2,25 @@ import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import commonStyles from '../components/common';
 import {Card, Input} from 'react-native-elements';
-import {useNavigation} from '@react-navigation/native';
+import {useNavigation, NavigationContainer} from '@react-navigation/native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+
+import Quiz from '../components/Quiz';
+
+const Tab = createMaterialTopTabNavigator();
+
+function QuizTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Quiz-1" component={Quiz} />
+      <Tab.Screen name="Quiz-2" component={Quiz} />
+      <Tab.Screen name="Quiz-3" component={Quiz} />
+    </Tab.Navigator>
+  );
+}
 
 export default function QuizScreen() {
   return (
-    <View style={commonStyles.main}>
-      <Card>
-        <Text>にゅうりょくしてください</Text>
-        <Input>aaa</Input>
-      </Card>
-    </View>
+    <QuizTabs />
   );
 }
