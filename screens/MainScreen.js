cdcf116b7fcb53a63090fdd7063a3664776fc3ea
NIIKURA Shozo <niikura@nyam.co.jp>
2020-10-25 12:32:47 +0900

    refactor: 不要なコードを整理しました。

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -3,14 +3,13 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import commonStyles from '../components/common';
 import {useNavigation} from '@react-navigation/native';
 
-import storeRoot, {store, clearStore, setStore} from '../modules/store';
+import {store, clearStore} from '../modules/store';
 import questions from '../modules/questions';
 
 let score, setScore;
 
-// export default class MainScreen extends Component {
 export default function MainScreen() {
-  [score, setScore] = React.useState(store.score);
+  [score, setScore] = React.useState(null);
   const {navigate} = useNavigation();
   return (
     <View style={commonStyles.main}>
@@ -19,10 +18,9 @@ export default function MainScreen() {
         onPress={() => {setScore(null); navigate('Quiz')}} />
       <View>
         { score === null ?
-          <Text></Text> :
+          <Button onPress={onPressButton} title="スコア表示" /> :
           <Text>Score: {score}</Text>
         }
-        <Button onPress={onPressButton} title="スコア表示" />
         <Button onPress={handleClearButton} title="クリア" />
       </View>
     </View>
@@ -30,10 +28,6 @@ export default function MainScreen() {
 }
 
 function onPressButton() {
-  console.log(store)
-  if (store.count === undefined)
-    store.count = 0;
-  store.count += 1;
   calcScore();
 }
 
@@ -43,17 +37,10 @@ function handleClearButton() {
 }
 
 function calcScore() {
-  let correct = 0;
-  let newScore = 0;
-  if (Object.keys(store.checked).length === 0)
-    return;
+  if (Object.keys(store.checked).length === 0) return;
 
-  questions().forEach(q => {
-    if(q.correctAnswer === store.checked[q.title]) {
-      correct += 1;
-    }
-  });
-  newScore = parseInt(100 * (correct / questions().length));
+  let correct = Object.values(store.correct).filter(v => v).length
+  let newScore = parseInt(100 * (correct / questions().length));
   store.score = newScore;
   setScore(newScore);
 }
